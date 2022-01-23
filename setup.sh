5e0be10267f04a732a2caee8959a23a812ea54c7
pradolucas <santos.lucasprado@gmail.com>
2022-01-23 20:36:15 -0300

    oh-my-zsh exit script fix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
-# !/bin/bash
+#!/usr/bin/env bash
+
+# ln -sfn ~/dotfiles/scripts/.gitconfig ~/.gitconfig
+# ln -sfn ~/dotfiles/scripts/.zshrc ~/.zshrc
+# ln -sfn ~/dotfiles/scripts/neovim.vim ~/.config/nvim/init.vim
+# ln -sfn ~/dotfiles/scripts/lua ~/.config/nvim/lua
+# ln -sfn ~/dotfiles/scripts/.tmux.conf ~/.tmux.conf
 
 xargs sudo apt-get install <packages.txt
 # chsh -s $(which zsh)
@@ -6,7 +12,9 @@ mv ~/.config/nvim ~/.config/NVIM.BAK
 rm -rf "$HOME/.zinit"
 rm -rf "$HOME/.oh-my-zsh"
 
-curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source ./scripts/oh-my-zsh-install.sh
+rm .zshrc
 
 echo '[*] Stowing'
 stow -vSt ~ zsh tmux nvim
@@ -16,6 +24,6 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/the
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-mkdir /home/$USER/.local/bin/ mv nvinstall.sh; mv nvim.appimage $_
+mkdir --parents /home/$USER/.local/bin/; mv nvim.appimage $_
 
 echo "Finished! Reload your terminal to use the latest setup."
