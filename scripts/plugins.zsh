740e2e4ea2ff8e940a3ae17be182af99400c85b3
pradolucas <santos.lucasprado@gmail.com>
2022-01-25 14:21:47 -0300

    setup modifications

--- scripts/plugins.zsh
+++ scripts/plugins.zsh
@@ -1,4 +1,4 @@
-if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+if [[ ! -f "$HOME/.local/share/zinit/zinit.git/zinit.zsh" ]]; then
     print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
     command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
     command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
@@ -6,7 +6,7 @@ if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
         print -P "%F{160} The clone has failed.%f%b"
 fi
 
-source "$HOME/.zinit/bin/zinit.zsh"
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"  
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
@@ -40,4 +40,4 @@ ZSH_HIGHLIGHT_STYLES[path]='fg=cyan'
 ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=magenta'
 
 # Self update
-zinit self-update
+# zinit self-update
--- setup.sh
+++ setup.sh
@@ -1,24 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# ln -sfn ~/dotfiles/scripts/.gitconfig ~/.gitconfig
-# ln -sfn ~/dotfiles/scripts/.zshrc ~/.zshrc
-# ln -sfn ~/dotfiles/scripts/neovim.vim ~/.config/nvim/init.vim
-# ln -sfn ~/dotfiles/scripts/lua ~/.config/nvim/lua
-# ln -sfn ~/dotfiles/scripts/.tmux.conf ~/.tmux.conf
+xargs sudo apt-get install <scripts/packages.txt
 
-xargs sudo apt-get install <packages.txt
-# chsh -s $(which zsh)
 mv ~/.config/nvim ~/.config/NVIM.BAK
 rm -rf "$HOME/.zinit"
 rm -rf "$HOME/.oh-my-zsh"
 
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-source ./scripts/oh-my-zsh-install.sh
-rm .zshrc
+curl -fsSLhttps://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash 
+rm ~/.zshrc
 
 echo '[*] Stowing'
 stow -vSt ~ zsh tmux nvim
 
+ZSH_CUSTOM="/home/lodarp/.oh-my-zsh/custom/"
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
