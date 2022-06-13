a637bf61969ba06dd65bf0a69c586e511cd21ef3
Lucas Prado Santos <santos.lucasprado@gmail.com>
2022-06-13 11:54:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ set -e
 apt update -y
 apt upgrade -y
 apt install git
-mkdir -p "$1/.dotfiles"
-git clone https://github.com/pradolucas/.dotfiles --branch master "$1/.dotfiles"
-chmod +r+x "$1/.dotfiles/setup.sh"
+mkdir -p "$HOME/.dotfiles"
+git clone https://github.com/pradolucas/.dotfiles --branch master "$HOME/.dotfiles"
+chmod +r+x "$HOME/.dotfiles/setup.sh"
 
 echo "Installing"
-cd "$1/.dotfiles" && bash setup.sh
+cd "$HOME/.dotfiles" && bash setup.sh
